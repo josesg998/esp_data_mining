964317c7545091e1774d8d04a00f251754c29347
josesg998 <josesg998@gmail.com>
2024-04-13 23:42:40 -0300

    armado de base

---
+++ get_data.R
@@ -0,0 +1,50 @@
+# First, you need to have the devtools package installed
+#install.packages("devtools")
+# now, install the vdemdata package directly from GitHub
+#devtools::install_github("vdeminstitute/vdemdata")
+
+require("data.table")
+require("vdemdata")
+
+vdem <- vdemdata::vdem
+
+## Powell and Thyne coup dataset
+powell_and_thyne <- fread("http://www.uky.edu/~clthyn2/coup_data/powell_thyne_coups_final.txt")
+
+# write vdem as .pkg
+fwrite(vdem, "data/vdem.csv")
+fwrite(powell_and_thyne, "data/powell_and_thyne.csv")
+
+## Filter year
+vdem_post_1945 <- vdem[vdem$year >= 1950,]
+
+## remove duplicates by country and year in powell and thyne
+powell_and_thyne <- powell_and_thyne[!duplicated(powell_and_thyne[,c("country", "year")]),]
+
+
+#check <- data.frame("country"=sort(unique(powell_and_thyne$country)), 
+#                    "check"=sort(unique(powell_and_thyne$country)) %in% unique(vdem_post_1945$country_name))
+#check <- check[!check$check,]
+
+## arreglo paises
+mod_country <- list("Congo"                             = "Republic of the Congo",
+                    # dominica
+                    "Gambia"                            = "The Gambia",
+                    # grenada
+                    "Myanmar"                           = "Burma/Myanmar",
+                    "Swaziland"                         = "Eswatini",
+                    "Turkey"                            = "Türkiye",
+                    "Yemen People's Republic; S. Yemen" = "South Yemen",
+                    "Yemen Arab Republic; N. Yemen"     = "North Yemen")
+
+# modify country column in powell_and_thyne using mod_country
+powell_and_thyne$country <- ifelse(powell_and_thyne$country %in% names(mod_country), 
+                                       sapply(powell_and_thyne$country, function(x) mod_country[[x]]), 
+                                       powell_and_thyne$country)
+
+
+vdem_post_1945$coup <- 0
+vdem_post_1945[which(paste(vdem_post_1945$country_name, vdem_post_1945$year) 
+                %in% paste(powell_and_thyne$country, powell_and_thyne$year)),]$coup <- 1
+
+fwrite(vdem_post_1945, "data/vdem_coup.csv")
