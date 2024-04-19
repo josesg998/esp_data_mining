5e01eb1bbb6ec187a554cf1da0abc593f604a899
josesg998 <josesg998@gmail.com>
2024-04-19 11:54:13 -0300

    intento usar hyp tunning del paper

--- optimizacion_bayesiana/config.py
+++ optimizacion_bayesiana/config.py
@@ -2,26 +2,25 @@ from hyperopt import hp
 
 def config(modelo):    
     diccionario = {
-        'iteraciones': 10,
+        'iteraciones': 100,
         'CV':  [
         {'train':(1970,2009),'val':(2010,2011)},
         {'train':(1970,2011),'val':(2012,2013)},
         {'train':(1970,2013),'val':(2014,2015)},
         {'train':(1970,2015),'val':(2016,2017)},
         {'train':(1970,2017),'val':(2018,2019)}],
-        'input':'data/vdem_coup.pkg'
+        'input':'data/vdem_coup.pkl'
     }
     
     if modelo=='RF': # Random Forest
         from sklearn.ensemble import RandomForestClassifier        
         diccionario[modelo] =  {
-            'model':RandomForestClassifier(),
+            'model':RandomForestClassifier(n_jobs=-1),
             'output':'optimizacion_bayesiana/logs/OB_random_forest_log.csv',
             'space':{
-                'n_estimators':5,
-                'max_depth':    hp.uniformint('max_depth', 1, 4),
-                'max_features': hp.choice('max_features', [ 'sqrt', 'log2']),
-                'criterion':    hp.choice('criterion', ['gini', 'entropy'])
+                'n_estimators':1000,
+                'max_depth':    hp.uniformint('max_depth', 1, 15),
+                'max_features':hp.uniform('max_features', 0.1, 1.0),
                 },
             'trials':'optimizacion_bayesiana/trials/trials_RF.pkl'
         }
@@ -33,8 +32,8 @@ def config(modelo):
             'model':XGBClassifier(),
             'output':'optimizacion_bayesiana/logs/OB_XGB_log.csv',
             'space':{
-                'n_estimators':5,                
-                'max_depth':    hp.uniformint('max_depth', 1, 4)
+                'reg_lambda': hp.uniform('reg_lambda', 0.1, 10),
+                'learning_rate': hp.uniform('learning_rate', 0.01, 0.3),
                 },
             'trials':'optimizacion_bayesiana/trials/trials_XGB.pkl'
         }
