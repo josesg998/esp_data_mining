41da0f724b482519cd9db3397e7dfbe0e54420dc
josesg998 <josesg998@gmail.com>
2024-05-27 22:41:10 -0300

    adapto path a gcloud

--- corrida_final/train.py
+++ corrida_final/train.py
@@ -16,10 +16,12 @@ output         = config_ML['output']
 # %%
 
 # se toma archivo pickle, si no existe se crea desde el csv generado en el script de R
-df = pd.read_pickle('../data/vdem_coup_ML.pkl')
+df = pd.read_pickle('data/vdem_coup_ML.pkl')
 
 # drop non numeric columns for df pandas dataframe
 df = df.select_dtypes(include=['number'])
+
+df.columns[~df.columns.str.startswith('e_')]
 
 X = df.drop('coup', axis=1)
 y = df.set_index('year')['coup']
